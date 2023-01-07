c60f834c1eaab8ac7b97525c8ba2cc10cafd0570
oniszczak <aleks42@gmail.com>
2023-01-07 16:17:49 -0500

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by ALEKSANDER ONISZCZAK on 2023-01-07.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
